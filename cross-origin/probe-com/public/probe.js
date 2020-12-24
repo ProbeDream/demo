e0c8e6232f95993896488ff834eaab2946b4b9e0
ProbeDream <gump1016@163.com>
2020-12-24 22:48:15 +0800

    probe.js注释的添加

--- cross-origin/probe-com/public/probe.js
+++ cross-origin/probe-com/public/probe.js
@@ -1,3 +1,4 @@
+/* 封装一个ajax */
 const ajax = (method, url) => {
   return new Promise((resolve, reject) => {
     const XHR = new XMLHttpRequest();
@@ -15,18 +16,25 @@ const ajax = (method, url) => {
   });
 };
 
+/* 
+  请求friends.json成功的时候执行resolve(XHR.response)
+  将响应的内容打印出来
+*/
 ajax('get', 'http://bb.com:8888/friends.json').then((response) => {
   console.log(response);
 });
 
+/* 封装成为一个jsonp */
 const jsonp = (url) => {
   return new Promise((resolve, reject) => {
-    const random = `probeJSONPCallbackName ${Math.random()}`;
+    /* 保证函数的唯一性 */
+    const random = `probeJSONPCallbackName${Math.random()}`;
     window[random] = (data) => {
       resolve(data);
     };
     const script = document.createElement('script');
     script.src = `${url}?callback=${random}`;
+    /* 为了防止每次都创建一个标签插入到网页中 执行完毕之后就删除对应的标签对象 */
     script.onload = () => {
       script.remove();
     };
@@ -37,6 +45,9 @@ const jsonp = (url) => {
   });
 };
 
+/* 
+  创建一个script并且指定对应的url
+*/
 jsonp('http://bb.com:8888/friends.js').then((data) => {
   console.log(data);
 });
