8891f62dc87f907bb791a47d623c214c7d6aa4f4
ProbeDream <gump1016@163.com>
2020-12-25 23:08:45 +0800

    写一个静态服务器

--- static-server/server.js
+++ static-server/server.js
@@ -8,12 +8,42 @@ if (!port) {
 }
 const server = http.createServer((request, response) => {
   let parseURL = url.parse(request.url, true);
-  let pathWidthQuery = request.url;
+  let pathWithQuery = request.url;
   let queryString = '';
-  if (pathWidthQuery.indexOf('?') >= 0) {
-      /* 查询字符串为 ?开始的地方开始截取pathWithQuery */
-    queryString = pathWidthQuery.substring(pathWidthQuery.indexOf('?'));
+  if (pathWithQuery.indexOf('?') >= 0) {
+    /* 查询字符串为 ?开始的地方开始截取pathWithQuery */
+    queryString = pathWithQuery.substring(pathWithQuery.indexOf('?'));
   }
+  let { pathname, query } = parseURL;
+  let { method } = request;
+  /* 文件路径根据路径名判断 如果说为/ 那么文件路径为"/index.html" 否则为路径名 */
+  const filePath = pathname === '/' ? '/index.html' : pathname;
+  const index = filePath.lastIndexOf('.');
+  const suffix = filePath.substring(index);
+  /* 定义一个文件类型 */
+  const fileTypes = {
+    '.html': 'text/html',
+    '.css': 'text/css',
+    '.js': 'text/javascript',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+  };
+  /* 
+    内容类型 根据后缀名设置 如果没有后缀名那么默认的为 text/html解析网页的类型
+  */
+  response.setHeader(
+    'Content-Type',
+    `${fileTypes[suffix] || 'text/html'};charset=utf-8`
+  );
+  let content;
+  try {
+    content = fs.readFileSync(`./public/${filepath}`);
+  } catch (error) {
+    content = '文件不存在';
+    response.statusCode = 404;
+  }
+  response.write(content);
+  response.end();
 });
 
 server.listen(port, () => {
