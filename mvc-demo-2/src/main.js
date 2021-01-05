84614c3868401f037ee482cabd62b472b7450d8f
ProbeDream <gump1016@163.com>
2021-01-05 17:42:47 +0800

    模块管理主文件的添加

---
+++ mvc-demo-2/src/main.js
@@ -0,0 +1,10 @@
+import './reset.css';
+import './global.css';
+
+import c from './app1.js';
+import d from './app2.js';
+import './app3.js';
+import './app4.js';
+
+c.init('#app1');
+d.init('#app2');
