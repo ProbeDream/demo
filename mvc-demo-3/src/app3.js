b3a4ef4f20ef75aa6d1c8f4fec2796bd83f58941
ProbeDream <gump1016@163.com>
2021-01-06 22:32:04 +0800

    模块三的添加

---
+++ mvc-demo-3/src/app3.js
@@ -0,0 +1,30 @@
+import './app3.css';
+import $ from 'jquery';
+
+const html = `
+    <section id="app3">
+        <div class="square"></div>
+    </section>
+`;
+
+const eventbus = $(window);
+console.log(eventbus.on);
+console.log(eventbus.off);
+console.log(eventbus.trigger);
+
+const $element = $(html).appendTo($('body > .page'));
+const $square = $('#app3 .square');
+const localKey = 'app3.active';
+
+const active = localStorage.getItem(localKey) === 'yes';
+
+$square.toggleClass('active', active);
+$square.on('click', () => {
+  if ($square.hasClass('active')) {
+    $square.removeClass('active');
+    localStorage.setItem(localKey, 'no');
+  } else {
+    $square.addClass('active');
+    localStorage.setItem("app3.active", 'yes');
+  }
+});
