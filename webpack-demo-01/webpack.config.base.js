72c4cae7a34408f412478620336351153fc2c5af
ProbeDream <gump1016@163.com>
2021-01-07 21:50:15 +0800

    修改base文件

--- webpack-demo-01/webpack.config.base.js
+++ webpack-demo-01/webpack.config.base.js
@@ -2,7 +2,6 @@ const htmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
