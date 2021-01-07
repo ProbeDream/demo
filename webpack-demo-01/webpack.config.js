a60d93e00979bb3c9be643c1c582de833c5ccde3
ProbeDream <gump1016@163.com>
2021-01-07 20:41:44 +0800

    配置webpack-dev-server和miniCSSExtractPlugin

--- webpack-demo-01/webpack.config.js
+++ webpack-demo-01/webpack.config.js
@@ -1,5 +1,6 @@
 let path = require('path');
 let htmlWebpackPlugin = require('html-webpack-plugin');
+let miniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
@@ -12,13 +13,21 @@ module.exports = {
       title: 'probedream',
       template: './src/assets/template.html',
     }),
+    new miniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+      chunkFilename: '[id].[contenthash].css',
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader','css-loader'],
+        use: [miniCssExtractPlugin.loader, 'css-loader'],
       },
     ],
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
   },
 };
