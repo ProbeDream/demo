33d80a80759e88df5b6199a629ed36d6e3cc2ddd
ProbeDream <gump1016@163.com>
2021-01-07 21:33:45 +0800

    添加webpack基础的配置

---
+++ webpack-demo-01/webpack.config.base.js
@@ -0,0 +1,17 @@
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: `[name].[contenthash].js`,
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      title: 'probedream',
+      template: './src/assets/template.html',
+    }),
+  ],
+};
