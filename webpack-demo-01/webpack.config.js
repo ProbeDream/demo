953fcfe64e222c993371332efcf446879b794f0b
ProbeDream <gump1016@163.com>
2021-01-07 15:49:12 +0800

    添加支持HTML的插件和配置

--- webpack-demo-01/webpack.config.js
+++ webpack-demo-01/webpack.config.js
@@ -1,5 +1,5 @@
 let path = require('path');
-
+let htmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
@@ -7,4 +7,8 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: `[name].[contenthash].js`,
   },
+  plugins: [new htmlWebpackPlugin({
+    title:"probedream",
+    template:"./src/assets/template.html"
+  })],
 };
