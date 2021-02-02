7af63d762319d2f0c6bba9b77af894fa96f7d258
ProbeDream <gump1016@163.com>
2021-02-03 02:45:53 +0800

    简化自定义钩子

--- react-demo/useUpdate.js
+++ react-demo/useUpdate.js
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from 'react';
 const [n, setN] = useState(0);
 
-const useX = (n) => {
+const useX = (fn, array) => {
   const [nUpdatedCount, setUpdated] = useState(0);
   useEffect(() => {
     setUpdated((x) => x + 1);
-  }, [n]);
-  return { nUpdatedCount };
+  }, array);
+  useEffect(() => {
+    if (nUpdatedCount > 1) {
+      fn();
+    }
+  }, [nUpdatedCount]);
 };
-const { nUpdatedCount } = useX(n);
-useEffect(() => {
-  if (nUpdatedCount > 1) {
-    console.log('n变化了');
-  }
-}, [nUpdatedCount]);
+useX(() => {
+  console.log(`变化成功了`);
+}, [n]);
