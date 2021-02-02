90597838a1a17b1afde21c6ecc8ee1b39639826d
ProbeDream <gump1016@163.com>
2021-02-03 01:48:48 +0800

    函数组件

---
+++ react-demo/example02.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+const App = (props) => {
+  const [n, setN] = useState(0);
+  const onClick = () => {
+    setN(n + 1);
+  };
+  return (
+    <div>
+      {n}
+      <button onClick={onClick}>+1</button>
+    </div>
+  );
+};
+
+export default App;
