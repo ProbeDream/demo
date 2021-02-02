b890e2a4af251ee5ec3aca6b72f916e296dae471
ProbeDream <gump1016@163.com>
2021-02-03 02:50:24 +0800

    将传入的数组阉割为静态数组

--- react-demo/useUpdate.js
+++ react-demo/useUpdate.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 const [n, setN] = useState(0);
 
-const useUpdate = (fn, array) => {
+const useUpdate = (fn, dep) => {
+  /* 对应的react不确定传入的dep是否是一个动态数组 数组长度不确定的数组  */
   const [count, setCount] = useState(0);
   useEffect(() => {
     setCount((x) => x + 1);
-  }, array);
+  }, [dep]);
   useEffect(() => {
     if (count > 1) {
       fn();
@@ -14,4 +15,4 @@ const useUpdate = (fn, array) => {
 };
 useUpdate(() => {
   console.log(`变化成功了`);
-}, [n]);
+}, n);
