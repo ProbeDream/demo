ab663c1f60c64eddbd616bb749d8bbc2777475ab
ProbeDream <gump1016@163.com>
2021-02-03 02:47:33 +0800

    简化代码

--- react-demo/useUpdate.js
+++ react-demo/useUpdate.js
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from 'react';
 const [n, setN] = useState(0);
 
-const useX = (fn, array) => {
-  const [nUpdatedCount, setUpdated] = useState(0);
+const useUpdate = (fn, array) => {
+  const [count, setCount] = useState(0);
   useEffect(() => {
-    setUpdated((x) => x + 1);
+    setCount((x) => x + 1);
   }, array);
   useEffect(() => {
-    if (nUpdatedCount > 1) {
+    if (count > 1) {
       fn();
     }
-  }, [nUpdatedCount]);
+  }, [count]);
 };
-useX(() => {
+useUpdate(() => {
   console.log(`变化成功了`);
 }, [n]);
