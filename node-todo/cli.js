d0dc5fb7a6da35d1ed0d4175f38ea447ad2f4eac
ProbeDream <gump1016@163.com>
2021-02-09 19:34:00 +0800

    将对应的index.js改为cli.js

---
+++ node-todo/cli.js
@@ -0,0 +1,25 @@
+const { program } = require('commander');
+const { add } = require('./index');
+program.version('0.0.1');
+
+program
+  .option('-d,--debug', 'output extra debugging')
+  .option('-s,--small', 'small pizza size')
+  .option('-p,--pizza-type<type>', 'flavour of pizza');
+
+program
+  .command('add')
+  .description('add a task')
+  .action((...args) => {
+    const words = args.slice(0, -1).join(' ');
+    add();
+    console.log(words);
+  });
+program
+  .command('clear')
+  .description('claer all tasks')
+  .action((...args) => {
+    console.log('this is clear');
+  });
+
+program.parse(process.argv);
