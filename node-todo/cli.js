c8e4e8655a691a2e39ed7a7a6fd98a1840bf901a
ProbeDream <gump1016@163.com>
2021-02-12 22:13:28 +0800

    添加用户操作的结果回调

--- node-todo/cli.js
+++ node-todo/cli.js
@@ -1,24 +1,40 @@
-const { program } = require('commander');
-const api = require('./index');
-program.version('0.0.1');
+#!/usr/bin/env node
+const program = require('commander');
+const api = require('./index.js');
+const pkg = require('./package.json');
 
-program
-  .option('-d,--debug', 'output extra debugging')
-  .option('-s,--small', 'small pizza size')
-  .option('-p,--pizza-type<type>', 'flavour of pizza');
-
+program.version(pkg.version);
 program
   .command('add')
   .description('add a task')
   .action((...args) => {
     const words = args.slice(0, -1).join(' ');
-    api.add(words);
+    api.add(words).then(
+      () => {
+        console.log('添加成功');
+      },
+      () => {
+        console.log('添加失败');
+      }
+    );
   });
 program
   .command('clear')
-  .description('claer all tasks')
-  .action((...args) => {
-    console.log('this is clear');
+  .description('clear all tasks')
+  .action(() => {
+    api.clear().then(
+      () => {
+        console.log('清除完毕');
+      },
+      () => {
+        console.log('清除失败');
+      }
+    );
   });
 
 program.parse(process.argv);
+
+if (process.argv.length === 2) {
+  // 说明用户直接运行 node cli.js
+  void api.showAll();
+}
