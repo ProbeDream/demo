f21419e0733fb35a1d9b955cea57d1aa6ca4bd4e
Gump-TUF <Gump1016@163.com>
2021-02-14 00:11:24 +0800

    测试文件的添加

--- node-todo/__test__/db.spec.js
+++ node-todo/__test__/db.spec.js
@@ -1,10 +1,31 @@
 const db = require('../db');
+const fs = require('fs');
+jest.mock('fs');
 
 describe('db', () => {
-  it('can red', () => {
-    expect(db.read instanceof Function).toBe(true);
+  
+  afterEach(() => {
+    fs.clearMocks();
   });
-  it('can write', () => {
-    expect(db.write instanceof Function).toBe(true);
+
+  it('can read', async () => {
+    const data = [{ title: 'hi', done: true }];
+    fs.setReadFileMock('/xxx', null, JSON.stringify(data));
+    const list = await db.read('/xxx');
+    expect(list).toStrictEqual(data);
+  });
+
+  it('can write', async () => {
+    let fakeFile;
+    fs.setWriteFileMock('/yyy', (path, data, callback) => {
+      fakeFile = data;
+      callback(null);
+    });
+    const list = [
+      { title: '刘亦菲', done: false },
+      { title: '妮妮', done: true },
+    ];
+    await db.write(list, '/yyy');
+    expect(fakeFile).toBe(JSON.stringify(list) + '\n');
   });
 });
